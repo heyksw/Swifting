dedb0cb82b6fa5120eec25f12875a89f2c3682c1
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-10-25 18:15:50 +0900

    Create Ch1_2.swift

---
+++ Ch1_2.swift
@@ -0,0 +1,16 @@
+let age: Int = 26
+
+print("김상우는 \(age)살입니다.")
+print("2022년에는 \(age+1)살입니다.")
+
+class Person{
+	var name: String = "ksw"
+    var age: Int = 26
+}
+
+let ksw: Person = Person()
+print(ksw)
+
+print("\n############\n")
+
+dump(ksw)
