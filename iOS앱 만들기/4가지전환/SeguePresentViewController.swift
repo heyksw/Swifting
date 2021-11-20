c28d7a3b3b1651c8396a8c7284803aec05a8d838
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-11-20 16:43:06 +0900

    Create SeguePresentViewController.swift

---
+++ iOS앱 만들기/4가지전환/SeguePresentViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SeguePresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
