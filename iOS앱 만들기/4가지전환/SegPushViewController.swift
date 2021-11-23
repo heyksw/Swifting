b0f4a12968e357952524757f369290f2951b398d
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-11-24 00:32:32 +0900

    다운 캐스팅, 프로퍼티 접근

--- iOS앱 만들기/4가지전환/SegPushViewController.swift
+++ iOS앱 만들기/4가지전환/SegPushViewController.swift
@@ -1,11 +1,25 @@
+//
+//  SegPushViewController.swift
+//  ScreenTransactionExample
+//
+//  Created by 김상우 on 2021/11/19.
+//
+
 import UIKit
 
 class SegPushViewController: UIViewController {
-
+    
     @IBOutlet weak var tapBackButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var name: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     
     @IBAction func tapBackButton(_ sender: Any) {
