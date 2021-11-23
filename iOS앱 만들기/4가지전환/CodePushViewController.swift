a156e274778c654dff35195b1fcc17d706c2075a
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-11-24 00:32:12 +0900

    다운캐스팅, 프로퍼티 접근

--- iOS앱 만들기/4가지전환/CodePushViewController.swift
+++ iOS앱 만들기/4가지전환/CodePushViewController.swift
@@ -1,10 +1,23 @@
+//
+//  CodePushViewController.swift
+//  ScreenTransactionExample
+//
+//  Created by 김상우 on 2021/11/20.
+//
+
 import UIKit
 
 class CodePushViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
         // Do any additional setup after loading the view.
     }
 
